1b4228397840afb2ba05816825dfefc417eaa3b1
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2025-05-05 11:41:29 -0300

    Add files via upload

---
+++ Aula03-JS/exer.js
@@ -0,0 +1,29 @@
+// declara uma variavel com a sua idade, verificar o tipo dela
+const idade = 37
+console.log(idade, typeof idade)
+
+// declara uma variavel com a sua comida preferida e atribuir um valor depois da declaração
+var comida_fav
+comida_fav = "sushi"
+
+//neste caso, vc so esta printando 2 coisas diferentes, :(
+console.log("minha lalalalalala", comida_fav)
+// concatenando as string :)
+console.log("minha lalalalalala " + comida_fav)
+// porem usando template string :)
+console.log(`minha lalalalalala ${comida_fav}`)
+
+
+// Declarar uma variavel e nao dar valor pra ela, sendo undefined
+
+// Declarar 5 variaveis diferentes sem valores
+
+// Declarar uma variavel contendo uma string
+
+// Declare uma varivel contendo um numero dentro de uma string
+
+// Declare uma variavel com seu nome, e outra com sobrenome, e concatene os dois, depois faça um template string dos 2
+
+// Coloque a seguinte frase em uma variavel: It´s time
+
+// verifique o tipo de todas as variaveis criadas
---
+++ Aula03-JS/script.js
@@ -0,0 +1,49 @@
+// console.log(1 + 2)
+
+// VARIAVEIS
+// Alocando uma informação no espaço de memória
+
+// var a = 1
+// let b = 2
+// const c = 5
+
+// var nome = "Lucas"
+// nome = "Lucas dias"
+// console.log(nome)
+
+var a = "8"
+var b = 1
+let fruta = "Morango"
+const maiorDeIdade = false
+
+// console.log(parseFloat(a) + b)
+
+// console.log(typeof a, typeof b, typeof fruta, typeof maiorDeIdade)
+
+// +, - , *, /
+
+var preco = 20
+var quantidade = 5
+var total = preco * quantidade + " - " + "Este é o resultado da conta"
+var total = `${preco * quantidade} - Este é o resultado da conta`
+
+// console.log(total)
+
+var sobrenome, cidade, ano, taVivo
+sobrenome = "Pereira"
+cidade = "Rio de Janeiro"
+ano = 2025
+taVivo = true
+
+// console.log(sobrenome, cidade, ano, taVivo)
+// console.log(typeof sobrenome, typeof cidade, typeof taVivo)
+// console.log(typeof ano)
+
+//concatenação de strings
+console.log("Meu nome é " + sobrenome + "," + " eu moro no " + cidade + " e o ano é " + ano)
+
+//template string
+console.log(`Meu nome é ${sobrenome}, eu moro no ${cidade} e o ano é ${ano}`)
+
+
+
